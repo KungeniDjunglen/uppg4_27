6591cd30c3c08f287d73ecad91d8aceb9e3f9df1
TheBlueSoul5188 <Admin@TEB127004>
2022-10-21 10:13:53 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal ");
+            string heltal = Console.ReadLine();
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                switch (siffra)
+                {
+                    case 9:
+                        siffra = 0;
+                        break;
+                    default:
+                        siffra++;
+                        break;
+
+                }
+                Console.Write(siffra);
+            }
+
+        }
+    }
+}
